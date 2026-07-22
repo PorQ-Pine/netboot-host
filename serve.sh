f6f6936f53f22b6c50acb73a1251263152fe14c6
Nicolas Mailloux <nicolecrivain@gmail.com>
2026-07-22 00:00:45 -0400

    Dropbear

--- serve.sh
+++ serve.sh
@@ -2,5 +2,11 @@
 
 cd "$(dirname ""${0}"")"
 
+BOOT_DIR="nbd/boot"
+RSA_KEY="${BOOT_DIR}/rsa_hkey"
+if [ ! -e "${RSA_KEY}" ]; then
+	dropbearkey -t rsa -f "${RSA_KEY}"
+fi
+
 nbdkit -r file nbd/rootfs.squashfs -p 10809
-nbdkit --filter=cow --filter=partition linuxdisk nbd/boot type=ext4 size=1G -p 10810 partition=1
+nbdkit -r --filter=partition linuxdisk nbd/boot type=ext4 size=1G -p 10810 partition=1

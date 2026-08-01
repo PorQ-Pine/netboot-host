fbdef8d7b50d270a0d36173b9013f56eb456bdeb
Nicolas Mailloux <nicolecrivain@gmail.com>
2026-08-01 10:29:03 -0400

    Fetch firmware blob if needed

--- serve.sh
+++ serve.sh
@@ -4,8 +4,12 @@ cd "$(dirname ""${0}"")"
 
 BOOT_DIR="nbd/boot"
 RSA_KEY="${BOOT_DIR}/rsa_hkey"
+FIRMWARE_BLOB="${BOOT_DIR}/firmware.squashfs"
 if [ ! -e "${RSA_KEY}" ]; then
 	dropbearkey -t rsa -f "${RSA_KEY}"
+fi
+if [ ! -e "${FIRMWARE_BLOB}" ]; then
+	wget -O "${FIRMWARE_BLOB}" "https://github.com/PorQ-Pine/firmware/raw/refs/heads/main/wifi_bt/firmware.squashfs"
 fi
 
 nbdkit -r file nbd/rootfs.squashfs -p 10809
